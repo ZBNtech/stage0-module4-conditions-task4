08f16b3000758fbd202b2c57466c87c5b1314988
schedulerz <thisisabilkair.zhakupov@gmail.com>
2022-08-04 21:08:00 +0600

    Number #1

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -1,6 +1,9 @@
 package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
+    int result;
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        result = first > second ? 10 : -10;
+        System.out.println(result);
     }
 }
